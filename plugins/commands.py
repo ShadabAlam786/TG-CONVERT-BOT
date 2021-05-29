6bb182bbe85015927596d7a5d9bb643fb02663ef
ShadabAlam786 <78970265+ShadabAlam786@users.noreply.github.com>
2021-05-29 09:25:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,8 +11,8 @@ from Tools.Download import download
 
 
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+my_father = "https://t.me/Shadab_Alam"
+support = "https://telegram.dog/MSBOTCREATERS"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +20,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("👨🏻‍💻 My Father", url=my_father), InlineKeyboardButton("🔍 Support Channel", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
@@ -43,7 +43,7 @@ async def about(c, m):
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
 
-@Client.on_message(Filters.command(["converttovideo"]))
+@Client.on_message(Filters.command(["CTV"]))
 async def video(c, m):
 
   if Config.BOT_PWD:
@@ -61,7 +61,7 @@ async def video(c, m):
       else:
           await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
 
-@Client.on_message(Filters.command(["converttofile"]))
+@Client.on_message(Filters.command(["CTF"]))
 async def file(c, m):
 
   if Config.BOT_PWD:
